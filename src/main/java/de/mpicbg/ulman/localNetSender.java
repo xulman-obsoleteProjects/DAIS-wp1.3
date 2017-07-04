1ef6d367c7acab36026fa2948975bd19a7af8183
Vladimír Ulman <ulman@mpi-cbg.de>
2017-07-04 19:49:17 +0200

    initial commit of the skeleton plugins

---
+++ src/main/java/de/mpicbg/ulman/localNetSender.java
@@ -0,0 +1,41 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+package de.mpicbg.ulman;
+
+import org.scijava.command.Command;
+import org.scijava.plugin.Parameter;
+import org.scijava.plugin.Plugin;
+import org.scijava.app.StatusService;
+import org.scijava.log.LogService;
+import org.scijava.ui.UIService;
+import net.imagej.ops.OpService;
+import net.imagej.ImageJ;
+
+import org.zeromq.ZMQ;
+
+@Plugin(type = Command.class, menuPath = "DAIS>Local Network Image Sender")
+public class localNetSender implements Command
+{
+	@Parameter
+	private LogService log;
+
+	@Parameter
+	private StatusService statusService;
+
+	//@Override
+	public void initiate()
+	{
+		System.out.println("sender initiate");
+	}
+
+	@Override
+	public void run()
+	{
+		System.out.println("sender run");
+	}
+}
---
+++ src/test/java/de/mpicbg/ulman/openFiji.java
@@ -0,0 +1,37 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+package de.mpicbg.ulman;
+
+import net.imagej.ImageJ;
+
+public class openFiji
+{
+	public static void main(final String... args)
+	{
+		final ImageJ ij = new net.imagej.ImageJ();
+		ij.ui().showUI();
+		//ij.command().run(machineGTViaMarkers.class, true);
+
+/*
+		try {
+			//start up the worker class
+			final machineGTViaMarkers_Worker Worker
+				= new machineGTViaMarkers_Worker(ij.op(),ij.log());
+
+			//do the job
+			Worker.work(args);
+		}
+		catch (ImgIOException e) {
+			ij.log().error("machineGTViaMarkers error: "+e);
+		}
+
+		//and quit
+		ij.appEvent().quit();
+*/
+	}
+}
