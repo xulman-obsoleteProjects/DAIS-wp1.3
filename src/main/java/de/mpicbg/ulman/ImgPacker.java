22d9c526be5f922e9d1f90524149f00e1e8c3d03
Vladimír Ulman <ulman@mpi-cbg.de>
2017-07-07 12:00:39 +0200

    ADD, ImgPacker WIP: Sending+Receiving finished for the "header message",                     Added skeleton for general receiving of images.

--- src/main/java/de/mpicbg/ulman/ImgPacker.java
+++ src/main/java/de/mpicbg/ulman/ImgPacker.java
@@ -12,9 +12,12 @@ import net.imagej.ImgPlus;
 import net.imglib2.img.Img;
 import net.imglib2.img.WrappedImg;
 import net.imglib2.img.array.ArrayImg;
+import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.img.basictypeaccess.array.ArrayDataAccess;
 import net.imglib2.img.cell.CellImg;
+import net.imglib2.img.cell.CellImgFactory;
 import net.imglib2.img.planar.PlanarImg;
+import net.imglib2.img.planar.PlanarImgFactory;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.integer.ByteType;
 import net.imglib2.type.numeric.integer.ShortType;
@@ -23,6 +26,7 @@ import net.imglib2.type.numeric.integer.UnsignedShortType;
 import net.imglib2.type.numeric.real.DoubleType;
 import net.imglib2.type.numeric.real.FloatType;
 
+import java.util.StringTokenizer;
 import java.nio.ByteBuffer;
 import org.zeromq.ZMQ;
 
@@ -39,8 +43,10 @@ public class ImgPacker<T extends NativeType<T>>
 		//protocol version
 		String msg = new String("v1");
 
-		//pack the image metadata (including its dimensions)
-		msg += plusDataToString(imgP);
+		//dimensionality data
+		msg += " dimNumber " + imgP.numDimensions();
+		for (int i=0; i < imgP.numDimensions(); ++i)
+			msg += " " + imgP.dimension(i);
 
 		//decipher the voxel type
 		msg += " " + typeIDToString(typeToTypeID(imgP.firstElement()));
@@ -52,6 +58,9 @@ public class ImgPacker<T extends NativeType<T>>
 		{
 			msg += " ArrayImg ";
 			socket.send(msg.getBytes(), ZMQ.SNDMORE);
+
+			//send metadata and voxel data afterwards
+			packAndSendPlusData(imgP, socket);
 			packAndSendArrayImg((ArrayImg<T, ? extends ArrayDataAccess<?>>)img, socket);
 		}
 		else
@@ -59,39 +68,178 @@ public class ImgPacker<T extends NativeType<T>>
 		{
 			msg += " PlanarImg ";
 			throw new Exception("Cannot send PlaneImg images yet.");
+			//possibly add additional configuration hints to 'msg'
 			//socket.send(msg.getBytes(), ZMQ.SNDMORE);
-			//packAndSendArrayImg((PlanarImg<T,?>)img, socket);
+
+			//send metadata and voxel data afterwards
+			//packAndSendPlusData(imgP, socket);
+			//packAndSendPlanarImg((PlanarImg<T,?>)img, socket);
 		}
 		else
 		if (img instanceof CellImg)
 		{
 			msg += " CellImg ";
 			throw new Exception("Cannot send CellImg images yet.");
+			//possibly add additional configuration hints to 'msg'
 			//socket.send(msg.getBytes(), ZMQ.SNDMORE);
-			//packAndSendArrayImg((CellImg<T,?>)img, socket);
+
+			//send metadata and voxel data afterwards
+			//packAndSendPlusData(imgP, socket);
+			//packAndSendCellImg((CellImg<T,?>)img, socket);
 		}
 		else
 			throw new Exception("Cannot determine the type of image, cannot send it.");
 	}
 
 
-	// -------- support for the transmission of the envelope/initial message --------
-	private
-	String plusDataToString(final ImgPlus<T> imgP)
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	public
+	ImgPlus<?> receiveAndUnpack(final String header, final ZMQ.Socket socket) throws Exception
 	{
-		String msg = new String();
+		StringTokenizer headerST = new StringTokenizer(header, " ");
+		if (! headerST.nextToken().startsWith("v1"))
+			throw new Exception("Unknown protocol, expecting protocol v1.");
 
-		//dimensionality data
-		msg += " dimNumber " + imgP.numDimensions();
-		for (int i=0; i < imgP.numDimensions(); ++i)
-			msg += " " + imgP.dimension(i);
+		if (! headerST.nextToken().startsWith("dimNumber"))
+			throw new Exception("Incorrect protocol, expecting dimNumber.");
+		final int n = Integer.valueOf(headerST.nextToken());
 
-		//further meta data
-		msg += " imgPlusData__START";
+		//fill the dimensionality data
+		final int[] dims = new int[n];
+		for (int i=0; i < n; ++i)
+			dims[i] = Integer.valueOf(headerST.nextToken());
+
+		final String typeStr = new String(headerST.nextToken());
+		final String backendStr = new String(headerST.nextToken());
+
+		//envelope/header message is (mostly) parsed,
+		//start creating the output image of the appropriate type
+		Img<? extends NativeType<?>> img = null;
+
+		//create appropriate type and image variables
+		if (typeStr.startsWith("ByteType"))
+		{
+			if (backendStr.startsWith("ArrayImg"))
+				img = new ArrayImgFactory<ByteType>().create(dims, new ByteType());
+			else
+			if (backendStr.startsWith("PlanarImg"))
+				img = new PlanarImgFactory<ByteType>().create(dims, new ByteType());
+			else
+			if (backendStr.startsWith("CellImg"))
+				img = new CellImgFactory<ByteType>().create(dims, new ByteType());
+		}
+		else
+		if (typeStr.startsWith("UnsignedByteType"))
+		{
+			if (backendStr.startsWith("ArrayImg"))
+				img = new ArrayImgFactory<UnsignedByteType>().create(dims, new UnsignedByteType());
+			else
+			if (backendStr.startsWith("PlanarImg"))
+				img = new PlanarImgFactory<UnsignedByteType>().create(dims, new UnsignedByteType());
+			else
+			if (backendStr.startsWith("CellImg"))
+				img = new CellImgFactory<UnsignedByteType>().create(dims, new UnsignedByteType());
+		}
+		else
+		if (typeStr.startsWith("ShortType"))
+		{
+			if (backendStr.startsWith("ArrayImg"))
+				img = new ArrayImgFactory<ShortType>().create(dims, new ShortType());
+			else
+			if (backendStr.startsWith("PlanarImg"))
+				img = new PlanarImgFactory<ShortType>().create(dims, new ShortType());
+			else
+			if (backendStr.startsWith("CellImg"))
+				img = new CellImgFactory<ShortType>().create(dims, new ShortType());
+		}
+		else
+		if (typeStr.startsWith("UnsignedShortType"))
+		{
+			if (backendStr.startsWith("ArrayImg"))
+				img = new ArrayImgFactory<UnsignedShortType>().create(dims, new UnsignedShortType());
+			else
+			if (backendStr.startsWith("PlanarImg"))
+				img = new PlanarImgFactory<UnsignedShortType>().create(dims, new UnsignedShortType());
+			else
+			if (backendStr.startsWith("CellImg"))
+				img = new CellImgFactory<UnsignedShortType>().create(dims, new UnsignedShortType());
+		}
+		else
+		if (typeStr.startsWith("FloatType"))
+		{
+			if (backendStr.startsWith("ArrayImg"))
+				img = new ArrayImgFactory<FloatType>().create(dims, new FloatType());
+			else
+			if (backendStr.startsWith("PlanarImg"))
+				img = new PlanarImgFactory<FloatType>().create(dims, new FloatType());
+			else
+			if (backendStr.startsWith("CellImg"))
+				img = new CellImgFactory<FloatType>().create(dims, new FloatType());
+		}
+		else
+		if (typeStr.startsWith("DoubleType"))
+		{
+			if (backendStr.startsWith("ArrayImg"))
+				img = new ArrayImgFactory<DoubleType>().create(dims, new DoubleType());
+			else
+			if (backendStr.startsWith("PlanarImg"))
+				img = new PlanarImgFactory<DoubleType>().create(dims, new DoubleType());
+			else
+			if (backendStr.startsWith("CellImg"))
+				img = new CellImgFactory<DoubleType>().create(dims, new DoubleType());
+		}
+		else
+			throw new Exception("Unsupported voxel type, sorry.");
+
+		if (img == null)
+			throw new Exception("Unsupported image backend type, sorry.");
+
+		//the core Img is prepared, lets extend it with metadata and fill with voxel values afterwards
+		//create the ImgPlus from it -- there is fortunately no deep coping
+		ImgPlus<?> imgP = new ImgPlus<>(img);
+		receiveAndUnpackPlusData((ImgPlus)imgP, socket);
+
+		//populate with voxel data
+		if (backendStr.startsWith("ArrayImg"))
+		{
+			receiveAndUnpackArrayImg((ArrayImg)img, socket);
+		}
+		else
+		if (backendStr.startsWith("PlanarImg"))
+		{
+			//read possible additional configuration hints from 'header'
+			//and fine-tune the img
+			throw new Exception("Cannot receive PlaneImg images yet.");
+			//receiveAndUnpackArrayImg((PlanarImg)img, socket);
+		}
+		else
+		if (backendStr.startsWith("CellImg"))
+		{
+			//read possible additional configuration hints from 'header'
+			//and fine-tune the img
+			throw new Exception("Cannot receive CellImg images yet.");
+			//receiveAndUnpackArrayImg((CellImg)img, socket);
+		}
+		else
+			throw new Exception("Unsupported image backend type, sorry.");
+
+		return imgP;
+	}
+
+
+	// -------- support for the transmission of the image metadata --------
+	private
+	void packAndSendPlusData(final ImgPlus<T> imgP, final ZMQ.Socket socket)
+	{
 		//TODO: use mPack because metadata are of various types (including Strings)
-		msg += " imgPlusData__END";
+		//send single message with ZMQ.SNDMORE
+	}
 
-		return msg;
+	private
+	void receiveAndUnpackPlusData(final ImgPlus<T> imgP, final ZMQ.Socket socket)
+	{
+		//TODO: use mPack because metadata are of various types (including Strings)
+		//read single message
 	}
 
 
@@ -231,6 +379,14 @@ public class ImgPacker<T extends NativeType<T>>
 		default:
 			throw new Exception("Unsupported voxel type, sorry.");
 		}
+	}
+
+	private
+	void receiveAndUnpackArrayImg(final ArrayImg<T,? extends ArrayDataAccess<?>> img, final ZMQ.Socket socket) throws Exception
+	{
+		//is there more messages coming?
+		while (socket.hasReceiveMore())
+			System.out.println("Received: " + new String(socket.recv()));
 	}
 
 
