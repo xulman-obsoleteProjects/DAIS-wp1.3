c808d3d05d6ecc2f4e95f74e11508d178bcb8b7e
Vladimír Ulman <ulman@mpi-cbg.de>
2017-12-05 19:30:20 +0100

    ADD: Pixel-type specific classes to fill ByteBuffer.

---
+++ scijava/src/main/java/de/mpicbg/ulman/imgtransfer/buffers/ByteSender.java
@@ -0,0 +1,17 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+package de.mpicbg.ulman.imgtransfer.buffers;
+
+import java.nio.ByteBuffer;
+
+public class ByteSender implements Sender
+{
+	public
+	void send(final ByteBuffer bufWrite, final Object arrayRead, int offset, int length)
+	{ bufWrite.put((byte[])arrayRead); }
+}
---
+++ scijava/src/main/java/de/mpicbg/ulman/imgtransfer/buffers/Sender.java
@@ -0,0 +1,15 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+package de.mpicbg.ulman.imgtransfer.buffers;
+
+import java.nio.ByteBuffer;
+
+public interface Sender
+{
+	void send(final ByteBuffer bufWrite, final Object arrayRead, int offset, int length);
+}
