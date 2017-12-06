860c7d1132fe204037791b0212fa006e3dba0812
Vladimír Ulman <ulman@mpi-cbg.de>
2017-12-06 10:58:10 +0100

    CHG: Socket interface talks now about connection between array and some generic end-point.

--- scijava/src/main/java/de/mpicbg/ulman/imgtransfer/sockets/Socket.java
+++ scijava/src/main/java/de/mpicbg/ulman/imgtransfer/sockets/Socket.java
@@ -7,10 +7,25 @@
  */
 package de.mpicbg.ulman.imgtransfer.sockets;
 
-import org.zeromq.ZMQ;
 import java.nio.ByteBuffer;
 
 public interface Socket
 {
-	void transmit(final ZMQ.Socket socket, final ByteBuffer buffer, final int sendOnlyFlags);
+	/**
+	 * Uni-directional handler to (repeatedly) send-or-receive basic-type array to/from somewhere.
+	 *
+	 * The basic-type might be first wrapped into a caller-supplied ByteBuffer (to obtain
+	 * independence on the basic type), but this might not always be necessary (will depend
+	 * on the other end-point of this transmission). The ByteBuffer is understood therefore
+	 * only as a auxiliary intermediate buffer and is a parameter here so that caller can
+	 * re-use it.
+	 *
+	 * It is assumed that only a portion at \e offset of \e length is considered from the
+	 * input/output \e array.
+	 *
+	 * Implementing classes are expected to create the opposite end-point, e.g. a network
+	 * socket, to the \e array, and implement the transmission between the two.
+	 */
+	void transmit(final Object array, int offset, int length,
+	              final ByteBuffer auxBuf, final int sendOnlyFlags);
 }
