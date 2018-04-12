1e50766544590a9d66d79e1c0816b23928abffb6
Vladimír Ulman <ulman@mpi-cbg.de>
2018-04-12 12:24:06 +0200

    ADD: ImgParams_t got enum with voxel types and convenience functions;      clean() -> clear()

--- C++/core/src/ImgParams.h
+++ C++/core/src/ImgParams.h
@@ -1,6 +1,7 @@
 #ifndef ImgParamsH
 #define ImgParamsH
 
+#include <stdexcept>
 #include <string>
 
 /**
@@ -29,8 +30,92 @@ typedef struct imgParams
 	std::string backendType;
 
 	//-----------
+	//convenience enum-ed variants of the voxelType variable
+	//BTW: useful for switch-statements
+	typedef enum voxelType {
+		Byte,
+		UnsignedByte,
+		Short,
+		UnsignedShort,
+		Long,
+		UnsignedLong,
+		Float,
+		Double
+	} voxelTypes;
+
+	//convenience convertor function: enum-ed variant of the voxelType variable
+	voxelTypes enumVoxelType() const
+	{
+		voxelTypes vt;
+		if (voxelType.find("Byte") != std::string::npos) vt = Byte;
+		else
+		if (voxelType.find("UnsignedByte") != std::string::npos) vt = UnsignedByte;
+		else
+		if (voxelType.find("Short") != std::string::npos) vt = Short;
+		else
+		if (voxelType.find("UnsignedShort") != std::string::npos) vt = UnsignedShort;
+		else
+		if (voxelType.find("Long") != std::string::npos) vt = Long;
+		else
+		if (voxelType.find("UnsignedLong") != std::string::npos) vt = UnsignedLong;
+		else
+		if (voxelType.find("Float") != std::string::npos) vt = Float;
+		else
+		if (voxelType.find("Double") != std::string::npos) vt = Double;
+		else
+			//undetermined type, complain!
+			throw new std::runtime_error::runtime_error(
+			  (std::string("Couldn't recognize voxel type: ")+voxelType).c_str() );
+
+		return vt;
+	}
+
+	//-----------
+	//convenience calculator function: how many Voxels is this image?
+	long howManyVoxels() const
+	{
+		if (dim == 0) return 0;
+
+		long cnt = sizes[0];
+		for (int i=1; i < dim; ++i)
+			cnt *= sizes[i];
+
+		return cnt;
+	}
+
+	//convenience calculator function: how many Bytes is this image?
+	long howManyBytes() const
+	{
+		long voxelSize;
+		switch (enumVoxelType())
+		{
+			//enumVoxelType() will throw exception if voxel type is not understood
+			case Byte:
+			case UnsignedByte:
+				voxelSize = 1;
+				break;
+			case Short:
+			case UnsignedShort:
+				voxelSize = 2;
+				break;
+			case Long:
+			case UnsignedLong:
+				voxelSize = 4;
+				break;
+			case Float:
+				voxelSize = 4;
+				break;
+			case Double:
+				voxelSize = 8;
+				break;
+		}
+
+		return (voxelSize * howManyVoxels());
+	}
+
+	//-----------
 	//performs clean up inside the structure
-	void clean()
+	void clear()
 	{
 		if (sizes != NULL) delete[] sizes;
 	}
@@ -39,7 +124,7 @@ typedef struct imgParams
 	//{ sizes = new int[n]; }
 
 	~imgParams()
-	{ clean(); }
+	{ clear(); }
 
 } imgParams_t;
 
