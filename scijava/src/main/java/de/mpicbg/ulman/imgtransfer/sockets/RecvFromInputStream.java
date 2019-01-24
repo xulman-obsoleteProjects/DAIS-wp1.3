5c4f173f26ea688883d83c9908b58ccb506ec1f9
Vladimír Ulman <ulman@mpi-cbg.de>
2019-01-24 17:22:34 +0100

    ADD: (inefficient) implementation of Sockets to/from Output/InputStreams

---
+++ scijava/src/main/java/de/mpicbg/ulman/imgtransfer/sockets/RecvFromInputStream.java
@@ -0,0 +1,61 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+package de.mpicbg.ulman.imgtransfer.sockets;
+
+import de.mpicbg.ulman.imgtransfer.buffers.Buffer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.ByteBuffer;
+
+public class RecvFromInputStream implements Socket
+{
+	final InputStream is;
+	final Buffer sender;
+
+	//a handle on a local buffer to be potentially re-used
+	ByteBuffer buf = null;
+
+	public
+	RecvFromInputStream(final InputStream _is, final Buffer _sender)
+	{
+		is = _is;
+		sender = _sender;
+	}
+
+
+	public
+	void transmit(final Object arrayWrite, int offset, int length,
+	              final int sendOnlyFlags)
+	{
+		//the optimal length of the aux ByteBuffer for the current data
+		final int arrayLength = sender.getElemSize()*length;
+
+		if (buf != null)
+		{
+			//buffer exist, is long enough?
+			//NB: must be limited to the exact length, otherwise ZMQ waits to fill it...
+			if (buf.limit() > arrayLength) buf.limit(arrayLength);
+			buf.rewind();
+		}
+		if (buf == null || buf.limit() < arrayLength)
+		{
+			//no buffer, or one with an inadequate length
+			buf = ByteBuffer.allocateDirect(arrayLength);
+		}
+
+		try {
+			//VERY QUICK AND VERY DIRTY! (and inefficient)
+			is.read(buf.array());
+			buf.rewind();
+			sender.recv(buf, arrayWrite, offset, length);
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ scijava/src/main/java/de/mpicbg/ulman/imgtransfer/sockets/SendToOutputStream.java
@@ -0,0 +1,47 @@
+/*
+ * To the extent possible under law, the ImageJ developers have waived
+ * all copyright and related or neighboring rights to this tutorial code.
+ *
+ * See the CC0 1.0 Universal license for details:
+ *     http://creativecommons.org/publicdomain/zero/1.0/
+ */
+package de.mpicbg.ulman.imgtransfer.sockets;
+
+import de.mpicbg.ulman.imgtransfer.buffers.Buffer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.ByteBuffer;
+
+public class SendToOutputStream implements Socket
+{
+	final OutputStream os;
+	final Buffer sender;
+
+	public
+	SendToOutputStream(final OutputStream _os, final Buffer _sender)
+	{
+		os = _os;
+		sender = _sender;
+	}
+
+
+	public
+	void transmit(final Object arrayRead, int offset, int length,
+	              final int sendOnlyFlags)
+	{
+		//ZMQ.Socket does not copy ByteBuffer into its own space;
+		//at the same time, it does not signal back if the data was transfered;
+		//so, we need to create an extra ByteBuffer for every individual transfer
+		final ByteBuffer buf = ByteBuffer.allocateDirect(sender.getElemSize()*length);
+
+		try {
+			sender.send(buf, arrayRead, offset, length);
+			buf.rewind();
+			//VERY QUICK AND VERY DIRTY! (and inefficient)
+			os.write(buf.array());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
